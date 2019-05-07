ee7e5c5e8974ea548132c6f34ca5105475671b94
bluej0e <rv.rodrigo.viola@gmail.com>
2019-05-08 01:34:03 +0200

    B1

--- js/main.js
+++ js/main.js
@@ -1,15 +1,42 @@
-jQuery(document).ready(function( $ ) {
 
-  var dir = "./img/product/";
-  var fileextension = ".jpg";
-  $.ajax({
-      //This will retrieve the contents of the folder if the folder is configured as 'browsable'
-      url: dir,
-      success: function (data) {
-          $(data).find("a:contains(" + fileextension + ")").each(function () {
-              var filename = this.href.replace(window.location.host, "").replace("http://", "");
-              $("#productFoto").append("<img src='" + dir + filename + "'>");
-          });
-      }
-  });
-});
+const gallery = document.querySelector('.gallery');
+const overlay = document.querySelector('.overlay');
+const overlayImage = overlay.querySelector('img');
+const overlayClose = overlay.querySelector('.close');
+
+function generateHtml([h, v]){
+  return`<div class="item hi${h} vi${v}"><img src="img/product/${randNum(40)}.jpg" /></div>`;
+}
+
+function randNum(limit){
+  return Math.floor(Math.random() * limit) + 1 ;
+}
+
+function handleClick(e){
+  const src = e.currentTarget.querySelector('img').src;
+  overlayImage.src = src;
+  overlay.classList.add('open');
+}
+function close(){
+  overlay.classList.remove('open');
+}
+
+const digits = Array.from({length: 12}, () =>
+[randNum(3), randNum(3)]).concat([[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]])
+
+const html = digits.map(generateHtml).join('');
+
+gallery.innerHTML = html;
+
+const items = document.querySelectorAll('.item');
+
+items.forEach(item => item.addEventListener('click', handleClick));
+
+overlayClose.addEventListener('click', close);
+overlay.addEventListener('click', close);
+overlay.addEventListener('key', close);
+
+$(document).keypress(function(e) {
+    if (e.keyCode === 27) {
+        close;
+    }});

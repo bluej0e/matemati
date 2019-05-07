d4d87bb16d554ed6b96da2c985daf449b855627a
bluej0e <rv.rodrigo.viola@gmail.com>
2019-05-07 21:51:50 +0200

    alpha

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 jQuery(document).ready(function( $ ) {
 
-  // Add your custom JS code here
-
+  var dir = "./img/product/";
+  var fileextension = ".jpg";
+  $.ajax({
+      //This will retrieve the contents of the folder if the folder is configured as 'browsable'
+      url: dir,
+      success: function (data) {
+          $(data).find("a:contains(" + fileextension + ")").each(function () {
+              var filename = this.href.replace(window.location.host, "").replace("http://", "");
+              $("#productFoto").append("<img src='" + dir + filename + "'>");
+          });
+      }
+  });
 });
